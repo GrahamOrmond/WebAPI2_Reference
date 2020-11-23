2646850395cc3dc2db2c9390c441fd2fc82c5347
Graham <grahamwormond@gmail.com>
2020-11-23 12:14:33 -0500

    Author CRUD - POST - PUT - DELETE

--- WebAPI2_Reference/API/Controllers/AuthorsController.cs
+++ WebAPI2_Reference/API/Controllers/AuthorsController.cs
@@ -19,18 +19,58 @@ namespace WebAPI2_Reference.API.Controllers
         private AuthorDAO _authorDAO = new AuthorDAO();
 
         // GET: api/Authors
-        public IQueryable<AuthorDTO> GetAuthors()
+        public IQueryable<AuthorDetailsDTO> GetAuthors()
         {
             var authors = _authorDAO.GetAllAuthors();
             return authors;
         }
 
         // GET: api/Authors/5
-        [ResponseType(typeof(AuthorDTO))]
+        [ResponseType(typeof(AuthorDetailsDTO))]
         public IHttpActionResult GetAuthor(int id)
         {
-            AuthorDTO author = _authorDAO.GetAuthor(id);
+            AuthorDetailsDTO author = _authorDAO.GetAuthor(id);
             if (author == null)
+                return NotFound();
+
+            return Ok(author);
+        }
+
+        // POST: api/Authors
+        [ResponseType(typeof(Author))]
+        public async Task<IHttpActionResult> PostAuthor(AuthorCreateDTO authorModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // create new author and return results
+            AuthorDetailsDTO newAuthor = await _authorDAO.AddAuthor(authorModel);
+            return CreatedAtRoute("DefaultApi", new { id = newAuthor.Id }, newAuthor);
+        }
+
+
+        // PUT: api/Authors/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutAuthor(int id, AuthorUpdateDTO authorUpdate)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // update author and get results
+            var authorResults = await _authorDAO.UpdateAuthorAsync(id, authorUpdate);
+            if(authorResults == null) // author not found
+                return NotFound();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Authors/5
+        [ResponseType(typeof(AuthorDetailsDTO))]
+        public async Task<IHttpActionResult> DeleteAuthor(int id)
+        {
+            // delete author from the database
+            var author = await _authorDAO.DeleteAuthorAsync(id);
+            if (author == null) // author was not found to delete
                 return NotFound();
 
             return Ok(author);
--- WebAPI2_Reference/API/DAO/AuthorDAO.cs
+++ WebAPI2_Reference/API/DAO/AuthorDAO.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Threading.Tasks;
 using WebAPI2_Reference.API.DTO;
+using WebAPI2_Reference.Data_Models;
 using WebAPI2_Reference.Models;
 
 namespace WebAPI2_Reference.API.DAO
@@ -13,30 +17,46 @@ namespace WebAPI2_Reference.API.DAO
 
         }
 
-        public IQueryable<AuthorDTO> GetAllAuthors()
+        public IQueryable<AuthorDetailsDTO> GetAllAuthors()
         {
             return from a in _db.Authors
-                   select new AuthorDTO()
+                   select new AuthorDetailsDTO()
                    {
                        Id = a.Id,
                        Name = a.Name,
                    };
         }
 
-        public AuthorDTO GetAuthor(int id)
+        public AuthorDetailsDTO GetAuthor(int id)
         {
             return _db.Authors
                 .Select(a =>
-                new AuthorDTO()
+                new AuthorDetailsDTO()
                 {
                     Id = a.Id,
                     Name = a.Name,
                 }).SingleOrDefault(a => a.Id == id);
+        }
+        
+        internal async Task<AuthorDetailsDTO> AddAuthor(AuthorCreateDTO authorModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AuthorDetailsDTO> UpdateAuthorAsync(int id, AuthorUpdateDTO authorUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AuthorDetailsDTO> DeleteAuthorAsync(int id)
+        {
+            throw new NotImplementedException();
         }
 
         internal void Dispose()
         {
             _db.Dispose();
         }
+
     }
 }
