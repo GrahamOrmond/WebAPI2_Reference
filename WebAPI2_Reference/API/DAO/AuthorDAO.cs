e1e244b641808815a3ac4938873f827d73a78bd4
Graham <grahamwormond@gmail.com>
2020-11-23 12:55:40 -0500

    Author/Book - CREATE - UPDATE - DELETE

--- WebAPI2_Reference/API/DAO/AuthorDAO.cs
+++ WebAPI2_Reference/API/DAO/AuthorDAO.cs
@@ -37,20 +37,75 @@ namespace WebAPI2_Reference.API.DAO
                     Name = a.Name,
                 }).SingleOrDefault(a => a.Id == id);
         }
-        
-        internal async Task<AuthorDetailsDTO> AddAuthor(AuthorCreateDTO authorModel)
+
+        public async Task<AuthorDetailsDTO> AddAuthor(AuthorCreateDTO authorModel)
         {
-            throw new NotImplementedException();
+            // create new author
+            Author newAuthor = new Author()
+            {
+                Name = authorModel.Name
+            };
+
+            // add author to database
+            _db.Authors.Add(newAuthor);
+            await _db.SaveChangesAsync();
+            return new AuthorDetailsDTO()
+            {
+                Id = newAuthor.Id,
+                Name = newAuthor.Name,
+            };
         }
 
         public async Task<AuthorDetailsDTO> UpdateAuthorAsync(int id, AuthorUpdateDTO authorUpdate)
         {
-            throw new NotImplementedException();
+            // get the author from the database
+            var author = _db.Authors.SingleOrDefault(a => a.Id == id);
+            if (author == null) // author not found
+                return null;
+
+            // update the author
+            author.Name = authorUpdate.Name;
+            _db.Entry(author).State = EntityState.Modified;
+
+            try // try to save changes
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) // failed to save changes
+            {
+                throw;
+            }
+
+            // return new instance of author details
+            return new AuthorDetailsDTO()
+            {
+                Id = author.Id,
+                Name = author.Name,
+            };
         }
 
         public async Task<AuthorDetailsDTO> DeleteAuthorAsync(int id)
         {
-            throw new NotImplementedException();
+            // get the author from the database
+            Author author = await _db.Authors.FindAsync(id);
+            if (author == null) // author not found
+                return null;
+
+            // delete the author from the database
+            _db.Authors.Remove(author);
+            await _db.SaveChangesAsync();
+
+            // return details of the author deleted
+            return new AuthorDetailsDTO()
+            {
+                Id = author.Id,
+                Name = author.Name
+            };
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _db.Authors.Count(e => e.Id == id) > 0;
         }
 
         internal void Dispose()
