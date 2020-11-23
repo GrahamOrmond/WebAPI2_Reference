5c472872fa4191c07f31f57dc07c5ec88863a837
Graham <grahamwormond@gmail.com>
2020-11-23 03:54:37 -0500

    GET starting point - get all books and book details - get all authors and author details

--- WebAPI2_Reference/API/Controllers/AuthorsController.cs
+++ WebAPI2_Reference/API/Controllers/AuthorsController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using WebAPI2_Reference.API.DAO;
+using WebAPI2_Reference.API.DTO;
 using WebAPI2_Reference.Data_Models;
 using WebAPI2_Reference.Models;
 
@@ -13,13 +15,32 @@ namespace WebAPI2_Reference.API.Controllers
     [Authorize]
     public class AuthorsController : ApiController
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+
+        private AuthorDAO _authorDAO = new AuthorDAO();
+
+        // GET: api/Authors
+        public IQueryable<AuthorDTO> GetAuthors()
+        {
+            var authors = _authorDAO.GetAllAuthors();
+            return authors;
+        }
+
+        // GET: api/Authors/5
+        [ResponseType(typeof(AuthorDTO))]
+        public IHttpActionResult GetAuthor(int id)
+        {
+            AuthorDTO author = _authorDAO.GetAuthor(id);
+            if (author == null)
+                return NotFound();
+
+            return Ok(author);
+        }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
+                _authorDAO.Dispose();
             }
             base.Dispose(disposing);
         }
--- WebAPI2_Reference/API/Controllers/BooksController.cs
+++ WebAPI2_Reference/API/Controllers/BooksController.cs
@@ -1,27 +1,41 @@
-﻿using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Net;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using WebAPI2_Reference.API.DAO;
 using WebAPI2_Reference.API.DTO;
-using WebAPI2_Reference.API.Filters;
-using WebAPI2_Reference.Data_Models;
-using WebAPI2_Reference.Models;
+using WebAPI2_Reference.Models;using System.Data.Entity;
+
 
 namespace WebAPI2_Reference.API.Controllers
 {
     [Authorize]
     public class BooksController : ApiController
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private BookDAO _bookDAO = new BookDAO();
+
+        // GET: api/Authors
+        public IQueryable<BookDTO> GetBooks()
+        {
+            return _bookDAO.GetAllBooks();
+        }
+
+        // GET: api/Books/5
+        [ResponseType(typeof(BookDetailDTO))]
+        public async Task<IHttpActionResult> GetBook(int Id)
+        {
+            BookDetailDTO book = await _bookDAO.GetBook(Id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
+                _bookDAO.Dispose();
             }
             base.Dispose(disposing);
         }
